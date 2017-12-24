a6d1f7951a6612f33f956812d96c3374ca81f160
Mikko Tapaninen <mikko.tapaninen@ridenuviz.com>
2017-12-24 10:51:45 +0200

    Added ObjectMapper

--- PePeP06/Pods/Target Support Files/Pods-PePeP06/Pods-PePeP06-frameworks.sh
+++ PePeP06/Pods/Target Support Files/Pods-PePeP06/Pods-PePeP06-frameworks.sh
@@ -102,10 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyBeaver/SwiftyBeaver.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyBeaver/SwiftyBeaver.framework"
 fi
