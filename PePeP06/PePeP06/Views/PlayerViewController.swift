68e0eef7fb21a2a78bb6677b9464f5438e9b167a
Mikko Tapaninen <mikko.tapaninen@ridenuviz.com>
2017-12-27 19:45:51 +0200

    Added category selection to Player view

--- PePeP06/PePeP06/Views/PlayerViewController.swift
+++ PePeP06/PePeP06/Views/PlayerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PlayerViewController: UIViewController {
+class PlayerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     // MARK: - Properties
     
     var player: Player?
@@ -18,6 +18,7 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var passesLabel: UILabel!
     @IBOutlet weak var yellowsLabel: UILabel!
     @IBOutlet weak var redsLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     
     // MARK: - UIViewController
@@ -25,15 +26,47 @@ class PlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let player = player {
-            navigationItem.title = player.first_name
-            
-            nameLabel.text      = playerFullName(player: player)
-            gamesLabel.text     = player.matches
-            goalsLabel.text     = player.goals
-            passesLabel.text    = player.assists
-            yellowsLabel.text   = player.warnings
-            redsLabel.text      = player.suspensions
+        navigationItem.title = player?.first_name
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        updateStats()
+    }
+    
+    
+    // MARK: - UITableViewDataSource
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    // MARK: - Methods
+    
+    func updateStats() {
+        guard let player = player else {
+            log.warning("No player data to update")
+            return
         }
+        
+        nameLabel.text      = playerFullName(player: player)
+        gamesLabel.text     = player.matches
+        goalsLabel.text     = player.goals
+        passesLabel.text    = player.assists
+        yellowsLabel.text   = player.warnings
+        redsLabel.text      = player.suspensions
     }
 }
